a1d65b9e75be48fa47958efdb0e0bc17e7d956fa
Diego <diegogutcat28@gmail.com>
2024-04-26 15:52:21 -0500

    added tab

--- app/components/product/Details.tsx
+++ app/components/product/Details.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { Chip, Divider } from '@nextui-org/react';
+import { Card, CardBody, Chip, Divider, Tab, Tabs } from '@nextui-org/react';
 import { useTheme } from 'next-themes';
 import ProductTable from './Table';
 
@@ -28,7 +28,33 @@ const Details = () => {
 
          <Divider className={`mt-1 mb-2 ${theme === 'dark' ? 'bg-primary' : 'bg-secondary'}`} />
          <div>
-            <ProductTable />
+            <div className="flex w-full flex-col">
+               <Tabs aria-label="Options">
+                  <Tab key="photos" title="Photos">
+                     <Card>
+                        <ProductTable />
+                     </Card>
+                  </Tab>
+                  <Tab key="music" title="Music">
+                     <Card>
+                        <CardBody>
+                           Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+                           ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                           reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                           pariatur.
+                        </CardBody>
+                     </Card>
+                  </Tab>
+                  <Tab key="videos" title="Videos">
+                     <Card>
+                        <CardBody>
+                           Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                           deserunt mollit anim id est laborum.
+                        </CardBody>
+                     </Card>
+                  </Tab>
+               </Tabs>
+            </div>
          </div>
       </div>
    );
