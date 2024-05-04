ee7dfbf24db5c6c5d0f8a29ca58f5c8fb5f16e35
Jose guzman <joseguzmandev@gmail.com>
2024-05-04 02:11:38 -0500

    mobile details

--- app/catalog/layout.tsx
+++ app/catalog/layout.tsx
@@ -1,16 +1,16 @@
 import CatalogNav from './components/CatalogNav';
 
 export default function LayoutCatalog({
-   children,
+  children,
 }: Readonly<{
-   children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-   return (
-      <div className="grid md:grid-cols-6 sm:grid-cols-1 gap-4 min-h-screen py-16 md:px-28 sm:px-5 bg-background">
-         <div className="col-span-1/4 md:block sm:hidden xs:hidden ">
-            <CatalogNav />
-         </div>
-         <div className="w-full md:col-span-5 sm:col-span-1">{children}</div>
+  return (
+    <div className="grid md:grid-cols-6 sm:grid-cols-1 gap-4 min-h-screen sm:py-16 xs:py-5 md:px-28 sm:px-5 bg-background">
+      <div className="col-span-1/4 md:block sm:hidden xs:hidden ">
+        <CatalogNav />
       </div>
-   );
+      <div className="w-full md:col-span-5 sm:col-span-1">{children}</div>
+    </div>
+  );
 }
--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -4,23 +4,30 @@ import Link from 'next/link';
 
 export default function Header() {
   return (
-    <header className="bg-primary sm:block xs:hidden">
-      <div className="flex w-full">
-        <div className="container bg-secondary w-2/3 py-1 left-diagonal" />
-        <div className="container xl:w-1/4 md:w-1/2 py-1">
-          <div className="flex justify-center text-center text-secondary font-bold">
-            <span>Frena seguro, frena Casul</span>
+    <header className="bg-primary">
+      <div className="sm:block xs:hidden">
+        <div className="flex w-full">
+          <div className="container bg-secondary w-2/3 py-1 left-diagonal" />
+          <div className="container xl:w-1/4 md:w-1/2 py-1">
+            <div className="flex justify-center text-center text-secondary font-bold">
+              <span>Frena seguro, frena Casul</span>
+            </div>
           </div>
+          <div className="container bg-secondary w-2/3 py-1 right-diagonal" />
         </div>
-        <div className="container bg-secondary w-2/3 py-1 right-diagonal" />
+        <div className="container mx-auto md:pb-5 sm:pb-6 flex justify-center relative">
+          <div className="w-1/4 flex justify-center">
+            <Link href="/">
+              <Image src="/images/logo.webp" width={100} height={100} alt={'Logo'} />
+            </Link>
+          </div>
+          <SearchBar />
+        </div>
       </div>
-      <div className="container mx-auto md:pb-5 sm:pb-6 flex justify-center relative">
-        <div className="w-1/4 flex justify-center">
-          <Link href="/">
-            <Image src="/images/logo.webp" width={100} height={100} alt={'Logo'} />
-          </Link>
-        </div>
-        <SearchBar />
+      <div className="xs:flex sm:hidden py-1 justify-center">
+        <Link href="/">
+          <Image src="/images/logo.webp" width={50} height={50} alt={'Logo'} />
+        </Link>
       </div>
     </header>
   );
