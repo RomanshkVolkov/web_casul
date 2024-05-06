2b7c815968809add130c559f2232009991dc5b00
Diego <diegogutcat28@gmail.com>
2024-05-06 11:44:30 -0500

    product cards now are pressable

--- app/components/shared/ProductCard.tsx
+++ app/components/shared/ProductCard.tsx
@@ -1,7 +1,7 @@
 import NextImage from 'next/image';
 import { Card, CardHeader, CardBody, Image, Tooltip } from '@nextui-org/react';
-import Link from 'next/link';
 import { url } from '../../url/url-utils';
+import { useRouter } from 'next/navigation';
 
 interface Props {
   id: number;
@@ -11,8 +11,10 @@ interface Props {
   image: string;
 }
 export default function ProductCard({ id, title, brand, shortInfo, image }: Props) {
+  const { push } = useRouter();
+
   return (
-    <Card className="rounded-xl">
+    <Card className="rounded-xl" isPressable onClick={() => push(url.product(id))}>
       <CardBody className="py-2 bg-white h-[300px] flex justify-center items-center">
         <Image
           as={NextImage}
@@ -31,17 +33,15 @@ export default function ProductCard({ id, title, brand, shortInfo, image }: Prop
           unoptimized
         />
       </CardBody>
-      <Link href={url.product(id)}>
-        <CardHeader className="p-4 flex-col items-start max-w-64 sm:max-w-none">
-          <small className="text-default-500">{shortInfo}</small>
-          <Tooltip content={brand} className="max-w-64" placement="bottom">
-            <small className="text-default truncate">{brand}</small>
-          </Tooltip>
-          <Tooltip content={title} className="max-w-64" placement="bottom">
-            <h4 className="font-bold text-large overflow-hidden max-h-14">{title}</h4>
-          </Tooltip>
-        </CardHeader>
-      </Link>
+      <CardHeader className="p-4 flex-col items-start max-w-64 sm:max-w-none">
+        <small className="text-default-500">{shortInfo}</small>
+        <Tooltip content={brand} className="max-w-64" placement="bottom">
+          <small className="text-default truncate">{brand}</small>
+        </Tooltip>
+        <Tooltip content={title} className="max-w-64" placement="bottom">
+          <h4 className="font-bold text-large overflow-hidden max-h-14 text-left">{title}</h4>
+        </Tooltip>
+      </CardHeader>
     </Card>
   );
 }
