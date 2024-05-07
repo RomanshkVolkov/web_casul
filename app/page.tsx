9abdf0a6eb270976209c8bb4b281500a44bfb38b
Jose guzman <joseguzmandev@gmail.com>
2024-05-07 09:29:53 -0500

    search container sm:hidden

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,9 @@ export default async function Home() {
   const widget = <WidgetFilters type="icon" />;
   return (
     <div className="flex min-h-screen flex-col items-center justify-start md:p-24 sm:p-6 xs:p-4 bg-background">
-      <SearchContainer type="input" widgetFiltersButton={widget} />
+      <div className="sm:hidden xs:inline-block w-full">
+        <SearchContainer type="input" widgetFiltersButton={widget} />
+      </div>
       <div className="md:container xs:w-full flex justify center">
         <ProductsCarousel title="Productos nuevos" products={data} />
       </div>
