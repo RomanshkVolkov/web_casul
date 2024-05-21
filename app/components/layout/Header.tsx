5f36e81b65543a6fb10806b4aa089ccfee4dbfae
Diego <diegogutcat28@gmail.com>
2024-05-21 10:13:33 -0500

    slogan changed

--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -1,32 +1,37 @@
-import Image from 'next/image';
-import SearchBar from './SearchBar';
-import Link from 'next/link';
+import Image from "next/image";
+import SearchBar from "./SearchBar";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <header className="bg-primary">
-      <div className="sm:block xs:hidden">
+      <div className="xs:hidden sm:block">
         <div className="flex w-full">
-          <div className="container bg-secondary w-2/3 py-1 left-diagonal" />
-          <div className="container xl:w-1/4 md:w-1/2 py-1">
-            <div className="flex justify-center text-center text-secondary font-bold">
-              <span>Frena seguro, frena Casul</span>
+          <div className="left-diagonal container w-2/3 bg-secondary py-1" />
+          <div className="container py-1 md:w-1/2 xl:w-1/4">
+            <div className="flex justify-center text-center font-bold text-secondary">
+              <span>Frene SEGURO, Frene CASUL</span>
             </div>
           </div>
-          <div className="container bg-secondary w-2/3 py-1 right-diagonal" />
+          <div className="right-diagonal container w-2/3 bg-secondary py-1" />
         </div>
-        <div className="container mx-auto md:pb-5 sm:pb-6 flex justify-center relative">
-          <div className="w-1/4 flex justify-center">
+        <div className="container relative mx-auto flex justify-center sm:pb-6 md:pb-5">
+          <div className="flex w-1/4 justify-center">
             <Link href="/">
-              <Image src="/images/logo.webp" width={100} height={100} alt={'Logo'} />
+              <Image
+                src="/images/logo.webp"
+                width={100}
+                height={100}
+                alt={"Logo"}
+              />
             </Link>
           </div>
           <SearchBar />
         </div>
       </div>
-      <div className="xs:flex sm:hidden py-1 justify-center">
+      <div className="justify-center py-1 xs:flex sm:hidden">
         <Link href="/">
-          <Image src="/images/logo.webp" width={50} height={50} alt={'Logo'} />
+          <Image src="/images/logo.webp" width={50} height={50} alt={"Logo"} />
         </Link>
       </div>
     </header>
